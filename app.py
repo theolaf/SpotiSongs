48a92247b940e6ddb317db3e0c3bc313086b07c3
Théo Lafforgue <theo.lafforgue@gmail.com>
2021-12-30 00:52:07 +0100

    fixed login so that multiple users can use the app at the same time

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, session, make_response
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_session import Session
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
+import uuid
 
 ###### GETTING ENV VARS ######
 client_id = os.getenv('CLIENT_ID')
@@ -11,23 +13,45 @@ scope = ["user-library-read", "user-top-read"]
 
 ###### CREATING THE FLASK APP AND SPOTIPY CLIENT ######
 app = Flask(__name__)
-app.secret_key = os.getenv('FLASK_SECRET').encode('utf8')
-sp_client = spotipy.client.Spotify()
+app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET').encode('utf8')
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+Session(app)
+
+caches_folder = './.spotify_caches/'
+if not os.path.exists(caches_folder):
+    os.makedirs(caches_folder)
+
+def session_cache_path():
+    return caches_folder + session.get('uuid')
 
 ###### HOME ######
 @app.route("/")
 def index():
-    if "token_info" not in session: #if no token is available in the flask session, show login page
-        return render_template("login.html")
-    else: #if a token is available in the flask session, show home page
-        try:
-            sp_client.set_auth(session.get('token_info').get('access_token')) #auth the spotipy client with the stored token
-            name = sp_client.me()["display_name"] #get user's name
-            top_tracks = [track["id"] for track in sp_client.current_user_top_tracks(limit=10)["items"]] #get user's top 10 tracks
-            last_saves = [track["track"]["id"] for track in sp_client.current_user_saved_tracks(limit=10)["items"]] #get user's last 10 saved tracks
-            return render_template("home.html", name=name, top_tracks=top_tracks, last_saves=last_saves)
-        except  spotipy.exceptions.SpotifyException as e: # if the token has expired, the user has to reconnect
-            return render_template("login.html")
+    if not session.get('uuid'): # if visitor unknown create a new id
+        session.permanent = False
+        session['uuid'] = str(uuid.uuid4())
+
+    cache_handler = spotipy.cache_handler.CacheFileHandler(cache_path=session_cache_path())
+    auth_manager = SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope, cache_handler=cache_handler)
+
+    if request.args.get("code"): # if redirected from spotify login
+        auth_manager.get_access_token(request.args.get("code"))
+        return redirect('/')
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()): # render login when no token found
+        return render_template("login.html", auth_url=auth_manager.get_authorize_url())
+
+    try:
+        sp_client = spotipy.Spotify(auth_manager=auth_manager)
+        name = sp_client.me()["display_name"] # get user's name
+        print(name)
+        top_tracks = [track["id"] for track in sp_client.current_user_top_tracks(limit=10)["items"]] # get user's top 10 tracks
+        last_saves = [track["track"]["id"] for track in sp_client.current_user_saved_tracks(limit=10)["items"]] # get user's last 10 saved tracks
+        return render_template("home.html", name=name, top_tracks=top_tracks, last_saves=last_saves)
+
+    except spotipy.exceptions.SpotifyException as e: # if the token has expired, the user has to reconnect
+        return render_template("login.html", auth_url=auth_manager.get_authorize_url())
 
 ###### AUTH PROTOCOL ######
 @app.route("/login")
